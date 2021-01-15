ebd72c298266fdad6eff8c7f017aa074300c8db8
Hal Lee <hal@lee.me>
2021-01-15 08:43:17 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 		.library(name: "OPML", targets: ["OPML"])
 	],
 	dependencies: [
-		.package(name: "Html", url: "https://github.com/pointfreeco/swift-html.git", from: "0.3.0")
+		.package(name: "Html", url: "https://github.com/hallee/swift-html.git", from: "0.3.0")
 	],
 	targets: [
 		.target(name: "OPML", dependencies: ["Html"]),
