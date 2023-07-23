c74cbc52ced259bb662f825df6b22e4d668ba8a5
Vithanco <klaus@vithanco.com>
2023-07-23 06:33:41 +0200

    Update OPMLEntryBuilder.swift
    changed Attribute to OPMLAttribute

--- Sources/OPML/Parser/OPMLEntryBuilder.swift
+++ Sources/OPML/Parser/OPMLEntryBuilder.swift
@@ -2,7 +2,7 @@ final class OPMLEntryBuilder {
 
 	/// What is displayed when an outliner opens the OPML file.
 	var text: String?
-	var attributes: [Attribute]?
+	var attributes: [OPMLAttribute]?
 	var children: [OPMLEntryBuilder]?
 
 	var entry: OPMLEntry? {
@@ -10,7 +10,7 @@ final class OPMLEntryBuilder {
 		return OPMLEntry(text: text, attributes: attributes, children: children?.compactMap { $0.entry })
 	}
 
-	init(text: String? = nil, attributes: [Attribute]? = nil, children: [OPMLEntryBuilder]? = nil) {
+	init(text: String? = nil, attributes: [OPMLAttribute]? = nil, children: [OPMLEntryBuilder]? = nil) {
 		self.text = text
 		self.attributes = attributes
 		self.children = children
