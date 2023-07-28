7e9f4a55e04f383d5aefb89cf1d98c891e0d8321
Vithanco <klaus@kneupner.de>
2023-07-28 12:39:31 +0200

    changed structure a bit

--- Tests/OPMLTests/Tests.swift
+++ Tests/OPMLTests/Tests.swift
@@ -1,10 +1,21 @@
 import XCTest
 @testable import OPML
 
+extension Bundle {
+    static func locateFirst(forResource: String, withExtension: String) -> URL? {
+        for b in Bundle.allBundles {
+            if let u = b.url(forResource: forResource, withExtension: withExtension) {
+                return u
+            }
+        }
+        return nil
+    }
+}
+
 class Tests: XCTestCase {
 
 	func testOPMLNested() {
-		guard let file = Bundle.module.url(forResource: "rsparser", withExtension: "opml") else {
+        guard let file = Bundle.module.url(forResource: "rsparser", withExtension: "opml") else {
 			XCTFail("Missing opml file")
 			return
 		}
@@ -13,6 +24,8 @@ class Tests: XCTestCase {
 			let parser = try OPMLParser(file: file)
 			let opml = try parser.parse()
 			XCTAssertEqual(opml.entries.flatMap { $0.children ?? [] }.count, 138)
+            XCTAssertEqual(opml.entries.count, 73)
+            
 
 			let programmingEntries = opml.entries.first(where: { $0.text == "Programming" })?.children
 			XCTAssertEqual(programmingEntries?.count ?? 0, 33)
